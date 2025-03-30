f62b36b3e5a1f60f941a327ef02886d784281680
Martin Jönsson <martin.t.joensson@gmail.com>
2025-03-30 19:47:50 +0200

    fixed spelling

--- src/components/Main/Body.jsx
+++ src/components/Main/Body.jsx
@@ -10,7 +10,7 @@ export const Body = () => {
       <Hero />
       <ImageContainer
         content={true}
-        heading="Få jobber gjort"
+        heading="Få jobbet gjort"
         text="Vi håller till på Värmdö och arbetar även på mer svårtillgängliga platser ute i skärgården."
         image="https://images.unsplash.com/photo-1586017198015-5cefee201cbd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
